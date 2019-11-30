c8367e73158f773d2a2b425171bdb12f4aaeb7a4
Anton Orlov <ion6431@gmail.com>
2019-11-30 20:28:02 +0300

    processor takes first log and writes it to file + ignore that file

--- consumer/processor.go
+++ consumer/processor.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 
 	kafka "github.com/segmentio/kafka-go"
 	_ "github.com/segmentio/kafka-go/gzip"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 const inputTopic = "Generated"
 
@@ -22,6 +30,18 @@ func main() {
 		MaxBytes:  10e7, // 10MB
 	})
 
+	// Open file to write to (0222 FileMode stands for write only unix permissions)
+	// https://en.wikipedia.org/wiki/File_system_permissions
+	f, err := os.OpenFile("output.txt", os.O_APPEND|os.O_WRONLY, 0222)
+	if err != nil {
+		fmt.Println("cannot open file to write to")
+		fmt.Println(err)
+	}
+
+	defer f.Close()
+
+	// countWrites := 0
+	fmt.Println("starting writting kafka logs to file")
 	for {
 		ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
 
@@ -30,6 +50,33 @@ func main() {
 			fmt.Println(err)
 		}
 
-		fmt.Println(string(m.Value))
+		// fmt.Println(string(m.Value))
+		// n, err := f.WriteString(string(m.Value) + "\n")
+		// check(err)
+		// fmt.Printf("success!\n%v\n", n)
+		// countWrites++
+		// if countWrites == 10 {
+		// 	f.Sync()
+		// 	break
+		// }
+
+		var obj interface{}
+		json.Unmarshal(m.Value, &obj)
+		decodedObject := obj.(map[string]interface{})
+		// fmt.Println(eventType)
+		// f.WriteString(eventType)
+		fmt.Println(decodedObject)
+		f.WriteString(string(m.Value))
+		break
+		for k, v := range decodedObject {
+			// switch vv := v.(type) {
+			if k == "event_type" {
+				fmt.Println(v)
+			}
+			// switch v.(type) {
+			// case string:
+			// 	fmt.Println(k, "is string")
+			// }
+		}
 	}
 }

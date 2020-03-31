931adbc20a36c7250549146b196ea9a9731092ea
Anton Orlov <ion6431@gmail.com>
2020-03-31 22:15:48 +0000

    bookmarks object to store

---
+++ pkg/models/booksmark_event_description.go
@@ -0,0 +1,13 @@
+package models
+
+// BookmarksEventDescription is an bookmark creation event
+// IsAdded shows if bookmark is being added or removed
+// (true  => added)
+// (false => removed)
+type BookmarksEventDescription struct {
+	EventTime string `json:"event_time"`
+	Username  string `json:"username"`
+	ID        string `json:"old"`
+	EventType string `json:"event_type"`
+	IsAdded   bool   `json:"is_added"`
+}

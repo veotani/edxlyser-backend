c39988c6a29b7ddcfca9326eb2852940fa296167
Anton Orlov <ion6431@gmail.com>
2020-03-31 22:15:37 +0000

    link event logs

---
+++ pkg/models/link_log.go
@@ -0,0 +1,15 @@
+package models
+
+// LinkLog is a definition of a log object with event type "edx.ui.lms.link_clicked"
+type LinkLog struct {
+	Username  string       `json:"username"`
+	EventType string       `json:"event_type"`
+	Time      string       `json:"time"`
+	Event     LinkEventLog `json:"event"`
+}
+
+// LinkEventLog is a definition of an event object within ProblemLog
+type LinkEventLog struct {
+	TargetURL  string `json:"target_url"`
+	CurrentURL string `json:"current_url"`
+}

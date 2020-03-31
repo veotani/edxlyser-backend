c9c9ca6fac8f5d8ea236c0915f15a1109897e3c5
Anton Orlov <ion6431@gmail.com>
2020-03-31 22:16:43 +0000

    links parser

---
+++ pkg/parsers/link.go
@@ -0,0 +1,23 @@
+package parsers
+
+import (
+	"encoding/json"
+	"kafka-log-processor/pkg/models"
+)
+
+// ParseLinkEvent gets log object (as string represented in bytes, as it's returned
+// from kafka) and returns object with links-only-related properties.
+func ParseLinkEvent(log []byte) (models.LinkEventDescription, error) {
+	var logObject models.LinkLog
+	err := json.Unmarshal(log, &logObject)
+	if err != nil {
+		return models.LinkEventDescription{}, err
+	}
+	return models.LinkEventDescription{
+		EventTime:  logObject.Time,
+		Username:   logObject.Username,
+		EventType:  logObject.EventType,
+		CurrentURL: logObject.Event.CurrentURL,
+		TargetURL:  logObject.Event.TargetURL,
+	}, nil
+}

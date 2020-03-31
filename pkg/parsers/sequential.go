cb4346ef19685d5489d85a155e8d4d3c8b7b5275
Anton Orlov <ion6431@gmail.com>
2020-03-31 22:16:38 +0000

    sequential parser

---
+++ pkg/parsers/sequential.go
@@ -0,0 +1,23 @@
+package parsers
+
+import (
+	"encoding/json"
+	"kafka-log-processor/pkg/models"
+)
+
+// ParseSequentialEvent gets log object (as string represented in bytes, as it's returned
+// from kafka) and returns object with sequential-only-related properties.
+func ParseSequentialEvent(log []byte) (models.SequentialMoveEventDescription, error) {
+	var logObject models.SequentialLog
+	err := json.Unmarshal(log, &logObject)
+	if err != nil {
+		return models.SequentialMoveEventDescription{}, err
+	}
+	return models.SequentialMoveEventDescription{
+		EventTime: logObject.Time,
+		Username:  logObject.Username,
+		EventType: logObject.EventType,
+		New:       logObject.Event.New,
+		Old:       logObject.Event.Old,
+	}, nil
+}

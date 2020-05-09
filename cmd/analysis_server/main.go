e48d5f4c161891064682c94d1bef9adb89c40957
Антон Орлов <veotani@Antons-MacBook-Air.local>
2020-05-09 19:10:33 +0300

    add video course catalogue

--- cmd/analysis_server/main.go
+++ cmd/analysis_server/main.go
@@ -30,13 +30,16 @@ func main() {
 	courseIDsWithLogsAndStructuresHandle := GetCourseIDsHandleFunction(&es)
 	usersRoutesCurversHandle := GetUsersRoutesCurves(*analysis)
 	usersWatchingsCurveHandle := GetUsersWatchingCurve(*analysis)
+	videoCatalogueByCourseHandle := GetVideoCatalogueByCourseHandle(&es)
 
 	http.HandleFunc("/course-ids-with-logs-and-structs", courseIDsWithLogsAndStructuresHandle)
 	http.HandleFunc("/course-routes", usersRoutesCurversHandle)
 	http.HandleFunc("/users-watchings", usersWatchingsCurveHandle)
+	http.HandleFunc("/video-ids-by-course", videoCatalogueByCourseHandle)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// GetUsersWatchingCurve returns points for video watching curve
 func GetUsersWatchingCurve(analysis analysers.Analyser) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		setupResponse(&w, r)
@@ -54,6 +57,33 @@ func GetUsersWatchingCurve(analysis analysers.Analyser) func(http.ResponseWriter
 		if err != nil {
 			log.Println(err)
 			return
+		}
+		fmt.Fprintf(w, string(b))
+	}
+}
+
+// GetVideoCatalogueByCourseHandle returns video ids of the course videos
+func GetVideoCatalogueByCourseHandle(es *database.ElasticService) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		setupResponse(&w, r)
+		if (*r).Method == "OPTIONS" {
+			return
+		}
+		course := r.URL.Query()["course"]
+		videos, err := es.GetUniqueStringFieldValuesInIndexWithFilter(
+			database.VideoEventDescriptionIndexName,
+			"video_id",
+			"course_id",
+			course[0],
+		)
+		if err != nil {
+			log.Printf("Error! Can't get unique videos for course: %v\n", err)
+			return
+		}
+
+		b, err := json.Marshal(videos)
+		if err != nil {
+			log.Printf("Error! Can't convert video list to json: %v\n", err)
 		}
 		fmt.Fprintf(w, string(b))
 	}
@@ -79,6 +109,7 @@ func GetCourseIDsHandleFunction(es *database.ElasticService) func(http.ResponseW
 	}
 }
 
+// GetUsersRoutesCurves returns users routes curves
 func GetUsersRoutesCurves(analysis analysers.Analyser) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		setupResponse(&w, r)
